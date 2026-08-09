a75f0a805d7cc34e8375a55677a257d28c2b4b23
NM-004 <enembarjo03@gmail.com>
2026-08-09 22:19:01 +0530

    better than before

--- src/base64Modal.ts
+++ src/base64Modal.ts
@@ -2,42 +2,58 @@ import { Modal, App, Setting } from 'obsidian';
 import { Buffer } from 'buffer';
 
 export class Base64Modal extends Modal {
+	private base64: string = '';
+	private utf_8: string = '';
+	private convertedValue: string = '';
+	private resultDisplayEl!: HTMLElement;
 	constructor(app: App) {
 		super(app);
-		this.setTitle('Base64 converter');
+	}
+	onOpen() {
+		const { contentEl } = this;
 
-		let norm_text = '';
-		let converted_string = '';
-		new Setting(this.contentEl)
-			.setClass('b64')
-			.setName('Base64')
-			.addTextArea((text) => {
-				text.onChange((value) => {
-					norm_text = value;
-				});
-			});
+		contentEl.createEl('h2', { text: 'Convert Base64 to UTF8' });
 
-		new Setting(this.contentEl).addButton((btn) => {
-			btn.setButtonText('Convert to normal');
-			btn.setCta();
-			btn.onClick(() => {
-				converted_string = Buffer.from(norm_text, 'base64').toString(
-					'utf-8',
-				);
-				this.contentEl.createEl('textarea', {
-					text: converted_string,
-				});
+		new Setting(contentEl).setName('Base64').addText((text) =>
+			text.onChange((value) => {
+				this.base64 = String(value) || '';
+				this.convertBase64();
+			}),
+		);
+		new Setting(contentEl).setName('utf-8').addText((text) => {
+			text.onChange((value) => {
+				this.utf_8 = String(value) || '';
+				this.convertUtf8();
 			});
 		});
-		new Setting(this.contentEl).addButton((btn) => {
-			btn.setButtonText('Convert to base64');
-			btn.setCta();
-			btn.onClick(() => {
-				converted_string = Buffer.from(norm_text).toString('base64');
-				this.contentEl.createEl('textarea', {
-					text: converted_string,
+
+		new Setting(contentEl)
+			.setName('Converted')
+			.setDesc('Live result')
+			.addText((text) => {
+				text.setValue('');
+				text.inputEl.readOnly = true;
+				text.inputEl.addEventListener('click', () => {
+					text.inputEl.select();
 				});
+				this.resultDisplayEl = text.inputEl;
 			});
-		});
+	}
+
+	private convertBase64() {
+		const normal = Buffer.from(this.base64, 'base64').toString('utf-8');
+		this.convertedValue = normal.toString();
+
+		if (this.resultDisplayEl instanceof HTMLInputElement) {
+			this.resultDisplayEl.value = this.convertedValue;
+		}
+	}
+
+	private convertUtf8() {
+		const converted = Buffer.from(this.utf_8, 'utf-8').toString('base64');
+		this.convertedValue = converted.toString();
+		if (this.resultDisplayEl.instanceOf(HTMLInputElement)) {
+			this.resultDisplayEl.value = this.convertedValue;
+		}
 	}
 }
